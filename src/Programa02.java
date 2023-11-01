746a67d2c7051e205c63e9dde88e391cfd699dfa
ohiagosouza <ohiagossouza@gmail.com>
2023-11-01 18:34:11 -0300

    first commit

---
+++ src/Programa02.java
@@ -0,0 +1,27 @@
+class Programa02 {
+    public static void main(String entrada[]) {
+        int n1, n2;
+        int mod, div;
+        double raiz, pot;
+        String msg = "";
+
+        n1 = 9;
+        n2 = 2;
+
+        mod = n1 % n2;
+        div = (int)n1 / (int)n2;
+        raiz = Math.sqrt(n1);
+        pot = Math.pow(n1,n2);
+
+        msg = "n1 = " + n1 + " e " + "n2" + " = " + n2 + "\n";
+        msg = msg + "resto da divisão de n1 por n2 = " + mod + "\n";
+        msg = msg + "quociente da divisão de n1 por n2 = " + div + "\n";
+        msg = msg + "raiz quadrada de n1 = " + raiz + "\n";
+        msg = msg + "potência de n1 e n2 = " + pot + "\n";
+        System.out.println(msg);
+//        System.exit(0);
+
+    }
+
+
+}
